71c75d25a0b50142162479473f936f391412cc21
Famousjasper <Famousjasper@users.noreply.github.com>
2017-10-19 17:27:24 +0200

    Fully implemented start window size settings

--- src/Preferences.java
+++ src/Preferences.java
@@ -42,10 +42,12 @@ public class Preferences extends JFrame {
         //Add tp to this frame
         this.add(tp);
 
+        //Create Layouts
+        GridLayout tabLayout = new GridLayout(20,0,2,2);
+        FlowLayout rowLayout = new FlowLayout();
         //'Advanced' tab content
-        FlowLayout advancedLayout = new FlowLayout();
-        this.advancedPref.setLayout(advancedLayout);
-        advancedLayout.setAlignment(FlowLayout.LEFT);
+        this.advancedPref.setLayout(tabLayout);
+        rowLayout.setAlignment(FlowLayout.LEFT);
         JCheckBox hwa = new JCheckBox("Hardware acceleration", Settings.HWACCEL);
         hwa.addItemListener(new ItemListener(){
             @Override
@@ -60,14 +62,69 @@ public class Preferences extends JFrame {
                 });
             }
         });
-        this.advancedPref.add(hwa);
+        JPanel hwPanel = new JPanel();
+        hwPanel.setLayout(rowLayout);
+        hwPanel.add(hwa);
+        this.advancedPref.add(hwPanel);
 
         //'Window' tab content
-        FlowLayout windowLayout = new FlowLayout();
-        this.windowPref.setLayout(windowLayout);
-        windowLayout.setAlignment(FlowLayout.LEFT);
-        JTextField winWidth = new JTextField("width");
-        this.windowPref.add(winWidth);
+        this.windowPref.setLayout(tabLayout);
+        
+        //Window width label
+        JPanel winxLabelPanel = new JPanel();
+        winxLabelPanel.setLayout(rowLayout);
+        JLabel winxLabel = new JLabel("Start width for main window");
+        winxLabelPanel.add(winxLabel);
+
+        //Window width textfield
+        JPanel winxTextPanel = new JPanel();
+        winxTextPanel.setLayout(rowLayout);
+        JTextField winWidth = new JTextField(Integer.toString(Settings.WINX), 15);
+        winWidth.setHorizontalAlignment(JTextField.RIGHT);
+        winWidth.addFocusListener(new FocusListener(){
+        
+            @Override
+            public void focusLost(FocusEvent e) {
+                Settings.WINX = Integer.valueOf(winWidth.getText());
+            }
+        
+            @Override
+            public void focusGained(FocusEvent e) {
+                
+            }
+        });
+        winxTextPanel.add(winWidth);
+
+        //Window height label
+        JPanel winyLabelPanel = new JPanel();
+        winyLabelPanel.setLayout(rowLayout);
+        JLabel winyLabel = new JLabel("Start height for main window");
+        winyLabelPanel.add(winyLabel);
+
+        //Window width textfield
+        JPanel winyTextPanel = new JPanel();
+        winyTextPanel.setLayout(rowLayout);
+        JTextField winHeight = new JTextField(Integer.toString(Settings.WINY), 15);
+        winHeight.setHorizontalAlignment(JTextField.RIGHT);
+        winHeight.addFocusListener(new FocusListener(){
+            
+                @Override
+                public void focusLost(FocusEvent e) {
+                    Settings.WINY = Integer.valueOf(winHeight.getText());
+                }
+            
+                @Override
+                public void focusGained(FocusEvent e) {
+                    
+                }
+            });
+        winyTextPanel.add(winHeight);
+
+        //Add items in order
+        this.windowPref.add(winxLabelPanel);
+        this.windowPref.add(winxTextPanel);
+        this.windowPref.add(winyLabelPanel);
+        this.windowPref.add(winyTextPanel);
 
         //Set close operation
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -75,6 +132,9 @@ public class Preferences extends JFrame {
         //Set size
         this.setSize(600, 600);
         this.setResizable(false);
+
+        //Set title
+        this.setTitle("Preferences");
 
 
         this.setLocationRelativeTo(null);
